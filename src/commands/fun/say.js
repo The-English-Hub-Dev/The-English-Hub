1e3544631e35b7fb5de9a667908812a0d228c738
graviscool <88552913+graviscool@users.noreply.github.com>
2022-05-27 09:28:56 -0700

    feat: add say command

---
+++ src/commands/fun/say.js
@@ -0,0 +1,27 @@
+const { Command, Args } = require('@sapphire/framework');
+const { Message } = require('discord.js');
+
+class SayCommand extends Command {
+	constructor(context, options) {
+		super(context, {
+			...options,
+			name: 'say',
+			description: 'Says things you tell the bot to say.',
+			aliases: ['echo'],
+			preconditions: ['Staff']
+		})
+	}
+
+	/**
+	 * 
+	 * @param { Message } message 
+	 * @param { Args } args
+	 */
+	async messageRun(message, args) {
+		const text = await args.restResult('string');
+		if (!text.success) return this.container.utility.errReply(message, 'You must provide something to say.');
+
+		return message.channel.send({content: text.value, allowedMentions: {users: [], roles: [], parse: []}});
+	}
+}
+module.exports = { SayCommand };
--- src/library/utility.js
+++ src/library/utility.js
@@ -1,5 +1,6 @@
 const { container } = require('@sapphire/pieces');
 const Sentry = require('@sentry/node');
+const { Message } = require('discord.js');
 
 class Utility {
 	constructor() {}
@@ -15,6 +16,16 @@ class Utility {
             `${type} exception with ID ${sentryID} sent to Sentry`
         );
 		return sentryID;
+	}
+
+	/**
+	 * 
+	 * @param { Message } message 
+	 * @param { String } error 
+	 */
+	async errReply(message, error) {
+		const reply = await message.reply({content: error, allowedMentions: {users: [], roles: [], parse: []}});
+		return setTimeout(() => reply.delete(), 3500);
 	}
 }
 
