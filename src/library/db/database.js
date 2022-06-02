81fecc00f7f6df863686159d90b0275ad5e317d1
graviscool <88552913+graviscool@users.noreply.github.com>
2022-06-01 18:30:35 -0700

    feat: finish up database stuff

--- src/library/db/database.js
+++ src/library/db/database.js
@@ -1,28 +1,34 @@
+const { container } = require('@sapphire/framework');
 const { DataSource } = require('typeorm');
 const { PunishmentEntity } = require('./entities/PunishmentEntity');
 
-const ds = new DataSource({
-    type: 'postgres',
-    connectTimeoutMS: 5000,
-    url: process.env.DATABASE_URL,
-    logging: ['error'],
-    entities: [],
-    cache: {
-        type: 'ioredis',
-        duration: 20000,
-        options: {
-            host: process.env.REDIS_HOST,
-            port: process.env.REDIS_PORT,
-        },
-    },
-});
 
 class Database {
-    async initializeDB() {
+    constructor() {
+        const ds = new DataSource({
+            type: 'postgres',
+            connectTimeoutMS: 5000,
+            url: process.env.DATABASE_URL,
+            ssl: {rejectUnauthorized: true},
+            logging: ['error'],
+            synchronize: true,
+            entities: [require('./entities/PunishmentEntity').punishmentEntity],
+            cache: {
+                type: 'ioredis',
+                duration: 20000,
+                options: {
+                    host: process.env.REDIS_HOST,
+                    port: process.env.REDIS_PORT,
+                },
+            },
+        });
+        ds.initialize().catch((e) => container.logger.error(e));
+
         this.typeorm = ds;
+        this.punishments = this.typeorm.getRepository("PunishmentEntity");
+    }
 
-        this.punishments = this.typeorm.getRepository(PunishmentEntity);
-
+    async initializeDB() {
         await this.typeorm.initialize();
     }
 }
---
+++ src/library/db/entities/PunishmentEntity.js
@@ -0,0 +1,58 @@
+const EntitySchema = require('typeorm').EntitySchema;
+const { container } = require('@sapphire/framework');
+const { DiscordSnowflake } = require('@sapphire/snowflake');
+require('reflect-metadata');
+
+const punishmentEntity = new EntitySchema({
+	name: "PunishmentEntity",
+	tableName: "punishments",
+	columns: {
+		punishment_id: {
+			primary: true,
+			type: 'text',
+		},
+		moderator_id: {
+			type: 'text',
+		},
+		user_id: {
+			type: 'text',
+		},
+		reason: {
+			type: 'text',
+		},
+		timestamp: {
+			type: 'timestamp',
+		},
+		type: {
+			type: 'text'
+		}
+	}
+});
+
+class Punishment {
+	/**
+	 * 
+	 * @param { String } userID 
+	 * @param { String } moderatorID 
+	 * @param { String } reason 
+	 * @param { String } type 
+	 */
+	constructor(userID, moderatorID, reason, type) {
+		const currentDate = new Date();
+		const p = {
+			punishment_id: String(DiscordSnowflake.generate({timestamp: currentDate.getTime()})),
+			moderator_id: moderatorID,
+			user_id: userID,
+			reason: reason,
+			timestamp: currentDate,
+			type: type
+		}
+		this.savePunishment(p);
+	}
+
+	async savePunishment(p) {
+		await container.database.punishments.save(p);
+	}
+}
+
+module.exports = { Punishment, punishmentEntity };
