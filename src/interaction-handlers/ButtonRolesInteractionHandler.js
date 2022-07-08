14f3912d90f802281fda0374627cafbdbb2dcb61
graviscool <88552913+graviscool@users.noreply.github.com>
2022-07-08 09:05:34 -0400

    feat: start on button roles

---
+++ src/interaction-handlers/ButtonRolesInteractionHandler.js
@@ -0,0 +1,41 @@
+const {
+    InteractionHandler,
+    InteractionHandlerTypes,
+} = require('@sapphire/framework');
+const {
+    ButtonInteraction, GuildMember,
+} = require('discord.js');
+
+class PeerMessageSendButtonHandler extends InteractionHandler {
+    constructor(ctx) {
+        super(ctx, { interactionHandlerType: InteractionHandlerTypes.Button });
+    }
+
+    /**
+     *
+     * @param { ButtonInteraction } interaction
+	 * @param { GuildMember } member
+     */
+    async run(interaction, member) {
+		switch (interaction.customId.split('-')[1]) {
+			case value:
+				
+				break;
+		
+			default:
+				break;
+		}
+    }
+
+    /**
+     *
+     * @param { ButtonInteraction } interaction
+     */
+    async parse(interaction) {
+		if (!interaction.customId.startsWith('role')) return this.none();
+
+		return this.some(interaction.member)
+    }
+}
+
+module.exports = { PeerMessageSendButtonHandler };
