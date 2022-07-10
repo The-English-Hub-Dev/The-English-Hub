0a35929f0fe8175a33c70c55d3dfd71771a8f8e1
graviscool <88552913+graviscool@users.noreply.github.com>
2022-07-10 13:44:27 -0400

    feat(index): add direct messages intent

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const client = new SapphireClient({
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_BANS,
         Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.DIRECT_MESSAGES
     ],
     defaultPrefix: prefix,
     loadMessageCommandListeners: true,
