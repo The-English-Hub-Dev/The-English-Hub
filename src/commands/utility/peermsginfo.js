68dd1f9ee0fcb28ecf9ada9613692994d36db6f7
graviscool <88552913+graviscool@users.noreply.github.com>
2022-07-23 14:12:00 -0400

    refactor(peermsginfo): add a period

--- src/commands/utility/peermsginfo.js
+++ src/commands/utility/peermsginfo.js
@@ -40,7 +40,7 @@ class PeerMsgInfoCommand extends Command {
         const embed = new MessageEmbed()
             .setTitle('Send a message to another member in the server')
             .setDescription(
-                `Click the button below if you would like to send a message to another member. It will prompt you for their ID and then the message you would like to send.\n\nAfter entering this information, the message will be sent to the staff to be approved. If it is approved, It will then be sent to the member you requested it to be sent to`
+                `Click the button below if you would like to send a message to another member. It will prompt you for their ID and then the message you would like to send.\n\nAfter entering this information, the message will be sent to the staff to be approved. If it is approved, It will then be sent to the member you requested it to be sent to.`
             )
             .setColor('#FF69B4')
             .setFooter({
