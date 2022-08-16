8cc78e573e48c6caa691d236ebb235bfea94bcef
graviscool <88552913+graviscool@users.noreply.github.com>
2022-08-16 10:09:04 -0700

    feat(ready): localize membercount in status

--- src/listeners/client/ready.js
+++ src/listeners/client/ready.js
@@ -24,27 +24,28 @@ class ReadyListener extends Listener {
         const hasRebooted = await this.container.redis.hget('tasks', 'restart');
         if (hasRebooted) {
             const [channelID, restartTime] = hasRebooted.split(':');
-            this.container.client.channels.cache
-                .get(channelID)
-                .send({
-                    embeds: [
-                        new MessageEmbed()
-                            .setDescription(
-                                `The bot restarted successfully in ${new DurationFormatter().format(
-                                    Date.now() - restartTime
-                                )}`
-                            )
-                            .setColor('GREEN'),
-                    ],
-                });
+            this.container.client.channels.cache.get(channelID).send({
+                embeds: [
+                    new MessageEmbed()
+                        .setDescription(
+                            `The bot restarted successfully in ${new DurationFormatter().format(
+                                Date.now() - restartTime
+                            )}`
+                        )
+                        .setColor('GREEN'),
+                ],
+            });
             await this.container.redis.hdel('tasks', 'restart');
         }
 
         setInterval(() => {
             const guild = client.guilds.cache.get('801609515391778826');
-            client.user.setActivity(` ${guild.memberCount} members`, {
-                type: 'WATCHING',
-            });
+            client.user.setActivity(
+                ` ${guild.memberCount.toLocaleString()} members`,
+                {
+                    type: 'WATCHING',
+                }
+            );
         }, 20000);
     }
 }
