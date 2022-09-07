6d8c8a6335659a5cfa8077d7e3199d250b3e9849
graviscool <88552913+graviscool@users.noreply.github.com>
2022-09-07 11:20:28 -0400

    feat(messagecommanderror): add command error listener and reply on

---
+++ src/listeners/command/messageCommandError.js
@@ -0,0 +1,30 @@
+const {
+    Listener,
+    Events,
+    MessageCommandErrorPayload,
+} = require('@sapphire/framework');
+
+class MessageCommandErrorListener extends Listener {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: Events.MessageCommandError,
+            event: Events.MessageCommandError,
+        });
+    }
+
+    /**
+     *
+     * @param { Error } error
+     * @param { MessageCommandErrorPayload } payload
+     */
+    async run(error, payload) {
+        this.container.logger.error(error);
+        payload.message.reply({
+            content: `An error occured: ${error.message}`,
+            allowedMentions: { users: [], roles: [], parse: [] },
+        });
+    }
+}
+
+module.exports = { MessageCommandErrorListener };
