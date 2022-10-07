51c3a2383ad74d1e7e22476b8db67601d3a0caa5
graviscool <88552913+graviscool@users.noreply.github.com>
2022-10-07 18:49:17 -0400

    feat(sentry): integrate sentry

--- src/listeners/command/messageCommandError.js
+++ src/listeners/command/messageCommandError.js
@@ -3,6 +3,7 @@ const {
     Events,
     MessageCommandErrorPayload,
 } = require('@sapphire/framework');
+const Sentry = require('@sentry/node');
 
 class MessageCommandErrorListener extends Listener {
     constructor(context, options) {
@@ -19,7 +20,11 @@ class MessageCommandErrorListener extends Listener {
      * @param { MessageCommandErrorPayload } payload
      */
     async run(error, payload) {
+        const capturedEx = Sentry.captureException(error, payload.context);
         this.container.logger.error(error);
+        this.container.logger.error(
+            `Error captured by Sentry with ID ${capturedEx}`
+        );
         return payload.message.reply({
             content: `An error occured: ${error.message}`,
             allowedMentions: { users: [], roles: [], parse: [] },
