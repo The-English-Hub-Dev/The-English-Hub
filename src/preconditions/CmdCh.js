d409dd31b188a71de71b49136d1f085fece6b483
graviscool <88552913+graviscool@users.noreply.github.com>
2022-12-31 12:45:43 -0800

    chore(config): add cmdChannels property as empty temp

---
+++ src/preconditions/CmdCh.js
@@ -0,0 +1,34 @@
+const { Precondition } = require('@sapphire/framework');
+const { Message, Permissions } = require('discord.js');
+const { cmdChannels } = require('../config.json');
+
+class CommandChPrecondition extends Precondition {
+    /**
+     *
+     * @param { Message } message
+     * @returns
+     */
+    async messageRun(message) {
+        if (
+            this.container.stores
+                .get('preconditions')
+                .get('Staff')
+                .messageRun(message)
+                .isOk()
+        )
+            return this.ok();
+
+        if (
+            (
+                await this.container.stores
+                    .get('preconditions')
+                    .get('Developer')
+                    .messageRun(message)
+            ).isOk()
+        )
+            return this.ok();
+		
+		else return cmdChannels.includes(message.channel.id) ? this.ok() : this.error({ message: 'This command can only be used in the following channels: <#'+cmdChannels.join('>, <#')+'>.' });
+    }
+}
+module.exports = { CommandChPrecondition };
