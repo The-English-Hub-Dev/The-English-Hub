a4b2bbc89f25dca8f68fe73cba943d7b061a998d
graviscool <88552913+graviscool@users.noreply.github.com>
2023-01-02 11:16:16 -0800

    feat(listeners): +messagecommandfinish

---
+++ src/listeners/command/messageCommandFinish.js
@@ -0,0 +1,32 @@
+const { Listener, Events, MessageCommand } = require('@sapphire/framework');
+const { Message } = require('discord.js');
+
+class MessageCommandFinishListener extends Listener {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: Events.MessageCommandFinish,
+            event: Events.MessageCommandFinish,
+        });
+    }
+
+    /**
+     *
+     * @param { Message } message
+     * @param { MessageCommand } command
+     */
+    async run(message, command) {
+        if (command.category.toLowerCase() === 'moderation') {
+            message.reply({
+                content:
+                    'Moderation commands are not fully implemented yet. Use with caution.',
+                allowedMentions: { repliedUser: false },
+            });
+        }
+        this.container.logger.info(
+            `Command ${command.name} executed by ${message.author.tag}.`
+        );
+    }
+}
+
+module.exports = { MessageCommandFinishListener };
