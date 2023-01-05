33022d9883ac5ec770965afb912da5c4a6931dab
graviscool <88552913+graviscool@users.noreply.github.com>
2023-01-05 13:38:21 -0800

    feat(cmds): +membercount!

---
+++ src/commands/utility/membercount.js
@@ -0,0 +1,77 @@
+const { time, TimestampStyles } = require('@discordjs/builders');
+const { Command, Args } = require('@sapphire/framework');
+const { Stopwatch } = require('@sapphire/stopwatch');
+const { Message, Permissions, MessageEmbed } = require('discord.js');
+
+class MembercountCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'membercount',
+            aliases: ['mc'],
+            preconditions: ['CmdCh'],
+            description: 'Gives you details about the server membercount.',
+        });
+    }
+
+    /**
+     *
+     * @param { Message } message
+     * @param { Args } _args
+     */
+    async messageRun(message, _args) {
+        const { guild } = message;
+
+        const mcRepl = await message.reply(
+            'Getting membercount details, please wait...'
+        );
+        const sw = new Stopwatch().start();
+
+        const bots = guild.members.cache.filter(
+            (member) => member.user.bot
+        ).size;
+
+        const staff = guild.members.cache.filter(
+            (member) =>
+                !member.user.bot &&
+                member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)
+        ).size;
+
+        const members =
+            guild.members.cache.filter((member) => !member.user.bot).size -
+            staff;
+
+        sw.stop();
+
+        const embed = new MessageEmbed()
+            .setTitle(`${guild.name} Membercount Details`)
+            .setColor('RANDOM')
+            .addFields([
+                { name: '**Members**', value: members, inline: true },
+                { name: '**Bots**', value: bots, inline: true },
+                { name: '**Staff**', value: staff, inline: true },
+            ])
+            .setDescription(
+                `Total Members: ${guild.memberCount.toLocaleString()}. Total Member growth in the last 24 hours(since ${time(
+                    Date.now() - 86400000,
+                    TimestampStyles.LongDateTime
+                )}): ${guild.members.cache
+                    .filter(
+                        (member) =>
+                            member.joinedTimestamp > Date.now() - 86400000
+                    )
+                    .size.toLocaleString()}`
+            )
+            .setFooter({
+                text: `Requested by ${message.author.tag}`,
+            })
+            .setTimestamp();
+
+        return mcRepl.edit({
+            content: `Membercount details fetched in ${sw}`,
+            embeds: [embed],
+        });
+    }
+}
+
+module.exports = { MembercountCommand };
