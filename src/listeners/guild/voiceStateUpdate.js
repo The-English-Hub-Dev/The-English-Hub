6168a6f9a2179bdc7aab612bb8b5ed9beac00182
graviscool <88552913+graviscool@users.noreply.github.com>
2023-07-30 10:46:31 -0700

    feat(voiceStateUpdate + config): impl room two creation system

--- src/listeners/guild/voiceStateUpdate.js
+++ src/listeners/guild/voiceStateUpdate.js
@@ -1,5 +1,6 @@
 const { Listener, Events } = require('@sapphire/framework');
-const { VoiceState } = require('discord.js');
+const { VoiceState, ChannelType } = require('discord.js');
+const { smallRoomParentID } = require('../../../config.json');
 
 class GuildMemberAddListener extends Listener {
     constructor(context, options) {
@@ -21,6 +22,30 @@ class GuildMemberAddListener extends Listener {
         this.container.logger.info(oldState);
         this.container.logger.info('newState');
         this.container.logger.info(newState);
+        await this.handleRoomTwoCreation(oldState, newState);
+    }
+
+    /**
+     *
+     * @param { VoiceState } oldState
+     * @param { VoiceState } newState
+     */
+    async handleRoomTwoCreation(oldState, newState) {
+        const twoRooms = oldState.guild.channels.cache.filter(
+            (channel) => channel.parent.id === smallRoomParentID
+        );
+
+        if (!oldState.channel && newState.channel.id === twoRooms.last().id) {
+            const newChannel = await oldState.guild.channels.create({
+                name: `Room 2.${
+                    Number(twoRooms.last().name.split(' ')[1].split('.')) + 1
+                }`,
+                type: ChannelType.GuildVoice,
+                parent: twoRooms.last().parent,
+                userLimit: 2,
+                reason: 'New 2 room vc creation as all current rooms are full.',
+            });
+        }
     }
 }
 
