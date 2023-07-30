004e5f2ac4297fa904bc065fef03c437aea2dc09
graviscool <88552913+graviscool@users.noreply.github.com>
2023-07-30 13:01:18 -0700

    Revert "chore(tasks): change time to 7s to test more efficiently"
    This reverts commit b448ccb53321e22bc496c4398cf4d0da3e10493a.

--- src/library/tasks.js
+++ src/library/tasks.js
@@ -105,7 +105,7 @@ class Tasks {
 
         const deleteInactiveTwo = setInterval(
             deleteAndRenameInactiveTwoRooms,
-            7000
+            5000
             // 60_000
         );
 
