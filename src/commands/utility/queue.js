fa61a32869184ecafbea9895ec316950cb3cf88e
graviscool <88552913+graviscool@users.noreply.github.com>
2023-11-20 11:42:05 -0800

    feat(queue): finish queue command!

---
+++ src/commands/utility/queue.js
@@ -0,0 +1,62 @@
+const { Command } = require('@sapphire/framework');
+const {
+    Message,
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+} = require('discord.js');
+const { DiscordSnowflake } = require('@sapphire/snowflake');
+class QueueCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'queue',
+            aliases: ['createqueue', 'create-queue'],
+            description: 'Creates a queue',
+            preconditions: ['Staff'],
+        });
+    }
+
+    /**
+     *
+     * @param { Message } message
+     * @returns
+     */
+    async messageRun(message) {
+        const queueSnowflake = DiscordSnowflake.generate();
+
+        const queueEmbed = new EmbedBuilder()
+            .setColor('Random')
+            .setTitle('Queue')
+            .setDescription(`Queue ID: ${queueSnowflake}\n\n**Users:** None`);
+
+        const queueActionRow = new ActionRowBuilder().addComponents([
+            new ButtonBuilder()
+                .setCustomId(`queue:join_${queueSnowflake}`)
+                .setLabel('Join')
+                .setStyle(ButtonStyle.Success),
+            new ButtonBuilder()
+                .setCustomId(`queue:leave_${queueSnowflake}`)
+                .setLabel('Leave')
+                .setStyle(ButtonStyle.Danger),
+            new ButtonBuilder()
+                .setCustomId(`queue:clear_${queueSnowflake}`)
+                .setLabel('Clear')
+                .setStyle(ButtonStyle.Secondary),
+        ]);
+
+        await message.channel.send({
+            embeds: [queueEmbed],
+            components: [queueActionRow],
+        });
+
+        message
+            .reply('Queue created!')
+            .then((m) => setTimeout(() => m.delete(), 5000));
+
+        return true;
+    }
+}
+
+module.exports = { QueueCommand };
---
+++ src/interaction-handlers/queueButtonHandler.js
@@ -0,0 +1,127 @@
+const {
+    InteractionHandler,
+    InteractionHandlerTypes,
+} = require('@sapphire/framework');
+const { ButtonInteraction, EmbedBuilder, Colors } = require('discord.js');
+const { staffRoles } = require('../../config.json');
+
+class QueueButtonHandler extends InteractionHandler {
+    constructor(ctx) {
+        super(ctx, { interactionHandlerType: InteractionHandlerTypes.Button });
+    }
+
+    /**
+     *
+     * @param { ButtonInteraction } interaction
+     */
+    async run(interaction) {
+        const command = interaction.customId.split('_')[0].split(':')[1];
+        const queueId = interaction.customId.split('_')[1];
+
+        const alreadyJoined = await this.container.redis.lpos(
+            `queue_${queueId}`,
+            interaction.user.id
+        );
+
+        switch (command) {
+            case 'join':
+                if (alreadyJoined !== null)
+                    return interaction.editReply({
+                        content: 'You have already joined this queue!',
+                        ephemeral: true,
+                    });
+                await this.container.redis.lpush(
+                    `queue_${queueId}`,
+                    interaction.user.id
+                );
+                await this.updateEmbed(interaction, queueId);
+
+                return interaction.editReply({
+                    content:
+                        'You have successfully joined this queue! Use the `Leave` button if you would like to leave.',
+                    ephemeral: true,
+                });
+            case 'leave':
+                if (alreadyJoined === null) {
+                    return interaction.editReply({
+                        content:
+                            'You have not joined this queue! Use the `Join` button if you would like to join.',
+                        ephemeral: true,
+                    });
+                }
+                await this.container.redis.lrem(
+                    `queue_${queueId}`,
+                    1,
+                    interaction.user.id
+                );
+                await this.updateEmbed(interaction, queueId);
+
+                return interaction.editReply({
+                    content:
+                        'You have successfully left this queue! Use the `Join` button if you would like to join again.',
+                    ephemeral: true,
+                });
+            case 'clear':
+                if (
+                    !staffRoles.some((role) =>
+                        interaction.member.roles.cache.has(role)
+                    )
+                ) {
+                    return interaction.editReply({
+                        content:
+                            'You do not have permission to clear this queue!',
+                        ephemeral: true,
+                    });
+                }
+                await this.container.redis.del(`queue_${queueId}`);
+                await this.updateEmbed(interaction, queueId);
+
+                return interaction.editReply({
+                    content: 'You have successfully cleared this queue!',
+                    ephemeral: true,
+                });
+            default:
+                break;
+        }
+    }
+
+    /**
+     *
+     * @param { ButtonInteraction } interaction
+     * @param { string } queueId
+     */
+    async updateEmbed(interaction, queueId) {
+        const oldEmbed = interaction.message.embeds[0];
+        const currentQueueUsersIDs = await this.container.redis.lrange(
+            `queue_${queueId}`,
+            0,
+            -1
+        );
+        const currentQueueUsers = currentQueueUsersIDs
+            .map((id, index) => `${index}: <@${id}>`)
+            .join('\n');
+
+        const newEmbed = new EmbedBuilder()
+            .setColor('Random')
+            .setTitle(oldEmbed.title)
+            .setDescription(
+                `Queue ID: ${queueId}\n\n**Users:** ${
+                    currentQueueUsers.length > 0 ? currentQueueUsers : 'None'
+                }`
+            );
+        return interaction.message.edit({ embeds: [newEmbed] });
+    }
+
+    /**
+     *
+     * @param { ButtonInteraction } interaction
+     */
+    async parse(interaction) {
+        if (!interaction.customId.startsWith('queue:')) return this.none();
+
+        await interaction.deferReply({ ephemeral: true });
+        return this.some();
+    }
+}
+
+module.exports = { QueueButtonHandler };
