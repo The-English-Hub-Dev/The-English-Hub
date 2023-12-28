46a93fba7c08b1680c936af5f68709d18a3f2419
graviscool <88552913+graviscool@users.noreply.github.com>
2023-12-27 20:47:44 -0800

    feat(triggerManager): add guild trigger support

--- src/library/managers/triggerManager.js
+++ src/library/managers/triggerManager.js
@@ -7,18 +7,33 @@ class TriggerManager {
     /**
      *
      * @param { Message } message
+     * @returns { boolean } if a trigger was activated or not on the specific message
      */
     async runTriggersOnMessage(message) {
         let triggered = false;
         if (message.author.bot) return false;
 
-        triggered = await this.runVivekHighlightTriggers(message);
+        triggered = await this.runGuildTriggers(message);
+        triggered = await this.runHlTriggers(message);
 
         return triggered;
     }
 
     /**
      * @param { Message } message
+     * @returns { Promise<boolean> } if triggered or not
+     */
+    async runHlTriggers(message) {
+        let hltriggered = false;
+
+        hltriggered = await this.runVivekHighlightTriggers(message);
+
+        return hltriggered;
+    }
+
+    /**
+     * @param { Message } message
+     * @returns { Promise<boolean> } if triggered or not
      */
     async runVivekHighlightTriggers(message) {
         const vivekhltriggers = await container.redis.lrange(
@@ -52,6 +67,32 @@ class TriggerManager {
                             `Failed to send a highlight message to ${vivekMember}`
                         )
                     );
+                triggered = true;
+            }
+        }
+
+        return triggered;
+    }
+
+    /**
+     * @param { Message } message
+     * @returns { Promise<boolean> } if triggered or not by guild triggers
+     */
+    async runGuildTriggers(message) {
+        let triggered = false;
+
+        const guildTriggers = Object.entries(
+            await container.redis.hgetall(`guildtriggers_${message.guild.id}`)
+        );
+
+        for (let i = 0; i < guildTriggers.length; i++) {
+            const trigger = guildTriggers[i][0];
+            const response = guildTriggers[i][1];
+            if (message.content.toLowerCase().includes(trigger.toLowerCase())) {
+                await message.reply({
+                    content: response,
+                    allowedMentions: { users: [], roles: [], parse: [] },
+                });
                 triggered = true;
             }
         }
