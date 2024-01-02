fd001e20033c467698500692b399d423354df269
graviscool <88552913+graviscool@users.noreply.github.com>
2024-01-02 12:54:44 -0800

    feat(guildmemberadd): add welcome dm functionality

--- src/listeners/guild/guildMemberAdd.js
+++ src/listeners/guild/guildMemberAdd.js
@@ -23,8 +23,9 @@ class GuildMemberAddListener extends Listener {
      * @param { GuildMember } member
      */
     async run(member) {
+        await this.sendWelcomeDM(member);
         await this.welcomeMember(member);
-        await this.sendMilestoneMessage(member);
+        await this.checkMilestone(member);
     }
 
     /**
@@ -44,7 +45,7 @@ class GuildMemberAddListener extends Listener {
      *
      * @param { GuildMember } member
      */
-    async sendMilestoneMessage(member) {
+    async checkMilestone(member) {
         if (member.guild.memberCount % 1000 != 0) return;
         const channel = member.guild.channels.cache.get(mainChannel);
         if (!channel || channel.type !== ChannelType.GuildText) return;
@@ -66,6 +67,30 @@ class GuildMemberAddListener extends Listener {
             embeds: [milestoneEmbed],
             allowedMentions: { users: [], roles: [], parse: [] },
         });
+    }
+
+    /**
+     * @param { GuildMember } member
+     */
+    async sendWelcomeDM(member) {
+        const welcomeDMEmbed = new EmbedBuilder()
+            .setTitle('Welcome to the English Hub')
+            .setColor(Colors.Aqua)
+            .setDescription(
+                `Hello, ${member} Welcome to a world of eclectic English learning. Chat with fellow members, share resources. Improve your skills through our English classes and active voice channels. If you need any help, use <#852953549112606821> to contact moderators.`
+            )
+            .setImage('https://imgur.com/1GZVpS1.png')
+            .setFooter(`Welcome!`)
+            .setTimestamp();
+
+        await member
+            .send({
+                content: `${member}`,
+                embeds: [welcomeDMEmbed],
+                allowedMentions: { users: [member.id], roles: [], parse: [] },
+            })
+            .catch(() => {});
+        return member.send('https://discord.gg/enghub').catch(() => {});
     }
 }
 
