9d08ff987eb06dfde11044c08f0f102e4a154dde
graviscool <88552913+graviscool@users.noreply.github.com>
2024-02-16 15:34:15 -0500

    feat(cmds): +bonk

---
+++ src/commands/fun/bonk.js
@@ -0,0 +1,42 @@
+const { Command, Args } = require('@sapphire/framework');
+const { Message } = require('discord.js');
+
+class BonkCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'bonk',
+            description: 'Bonk bonk.',
+            usage: '<member>',
+            aliases: [],
+            preconditions: ['Staff'],
+        });
+    }
+
+    /**
+     *
+     * @param { Message } message
+     * @param { Args } args
+     */
+    async messageRun(message, args) {
+        const rawMember = await args.pickResult('member');
+        if (rawMember.isErr())
+            return this.container.utility.errReply(
+                message,
+                'You must mention a member.'
+            );
+
+        const member = rawMember.unwrap();
+        if (message.deletable) await message.delete();
+
+        return message.channel.send({
+            content: `${message.author} bonked ${member} 🫂`,
+            allowedMentions: {
+                users: [member.id, message.author.id],
+                roles: [],
+                parse: [],
+            },
+        });
+    }
+}
+module.exports = { BonkCommand };
