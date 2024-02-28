dbd9947444ebaffa6c9cbbd7b0bc8bce312b64d3
graviscool <graviscool@users.noreply.github.com>
2024-02-28 04:02:41 +0000

    chore(formatting[auto]): run prettier on code

--- src/preconditions/StaffBanPerms.js
+++ src/preconditions/StaffBanPerms.js
@@ -19,7 +19,9 @@ class StaffBanPermsPrecondition extends Precondition {
         )
             return this.ok();
 
-        return message.member.roles.cache.has('1040897095055458344') ? this.ok() : this.error(); // crew mate role OR ADMIN
+        return message.member.roles.cache.has('1040897095055458344')
+            ? this.ok()
+            : this.error(); // crew mate role OR ADMIN
     }
 }
 module.exports = { StaffBanPermsPrecondition };
