ec7ce22e5f62597096f43f8b3e8a97885a0e7b8a
graviscool <88552913+graviscool@users.noreply.github.com>
2024-02-27 23:01:10 -0500

    feat(ban): only allow ban to be run by crewmate and above

---
+++ src/preconditions/StaffBanPerms.js
@@ -0,0 +1,25 @@
+const { Precondition } = require('@sapphire/framework');
+const { Message, Permissions } = require('discord.js');
+const { staffRoles, testingServerID } = require('../../config.json');
+
+class StaffBanPermsPrecondition extends Precondition {
+    /**
+     *
+     * @param { Message } message
+     * @returns
+     */
+    async messageRun(message) {
+        if (
+            (
+                await this.container.stores
+                    .get('preconditions')
+                    .get('Admin')
+                    .messageRun(message)
+            ).isOk()
+        )
+            return this.ok();
+
+        return message.member.roles.cache.has('1040897095055458344') ? this.ok() : this.error(); // crew mate role OR ADMIN
+    }
+}
+module.exports = { StaffBanPermsPrecondition };
