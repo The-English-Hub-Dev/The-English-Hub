76d3c2bf53fb0dee571f38c25ec12108150a3719
graviscool <88552913+graviscool@users.noreply.github.com>
2024-04-15 14:37:22 -0400

    chore: change config file

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        env: {
+            commonjs: true,
+            es2021: true,
+            node: true,
+        },
+        extends: 'eslint:recommended',
+        parserOptions: {
+            ecmaVersion: 'latest',
+        },
+        rules: {
+            'no-unused-vars': 'off',
+            'no-case-declarations': 'off',
+        },
+    },
+];
