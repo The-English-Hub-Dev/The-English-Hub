95b5653f4a65ec13d33bf44990f5498ddaa97e91
graviscool <graviscool@users.noreply.github.com>
2024-05-12 15:59:40 +0000

    chore(formatting[auto]): run prettier on code

--- src/listeners/command/messageCommandError.js
+++ src/listeners/command/messageCommandError.js
@@ -22,8 +22,10 @@ class MessageCommandErrorListener extends Listener {
     async run(error, payload) {
         await this.container.utility.exception(error, 'Command');
         if (error.message == 'Received one or more errors') {
-            this.container.logger.error('Logging in console because of multiple errors')
-            this.container.logger.error(error)
+            this.container.logger.error(
+                'Logging in console because of multiple errors'
+            );
+            this.container.logger.error(error);
         }
         return payload.message.reply({
             content: `An error occured: ${error.message}`,
