fea704dcb3c6937718b79eef8d0689385dbcedbf
graviscool <88552913+graviscool@users.noreply.github.com>
2024-07-09 23:10:33 -0700

    servermute: change mute perms to not include trusted

---
+++ src/preconditions/VcMutePerms.js
@@ -0,0 +1,41 @@
+const { Precondition } = require('@sapphire/framework');
+const { Message, Permissions } = require('discord.js');
+const { vcActionPerms, testingServerID } = require('../../config.json');
+
+class VcMutePermsPrecondition extends Precondition {
+    /**
+     *
+     * @param { Message } message
+     * @returns
+     */
+    async messageRun(message) {
+        if (
+            (
+                await this.container.stores
+                    .get('preconditions')
+                    .get('Admin')
+                    .messageRun(message)
+            ).isOk()
+        )
+            return this.ok();
+
+        if (
+            (
+                await this.container.stores
+                    .get('preconditions')
+                    .get('Staff')
+                    .messageRun(message)
+            ).isOk()
+        )
+            return this.ok();
+
+        return vcActionPerms.some(
+            (role) =>
+                message.member.roles.cache.has(role) &&
+                role.id !== '1234252770035630162'
+        )
+            ? this.ok()
+            : this.error();
+    }
+}
+module.exports = { VcMutePermsPrecondition };
