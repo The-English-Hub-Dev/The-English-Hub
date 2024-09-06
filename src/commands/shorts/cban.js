95f7633fa4dd0e0df064e043263a9b68f075b98e
graviscool <88552913+graviscool@users.noreply.github.com>
2024-09-06 19:10:13 -0400

    feat(cmds): + cban, cunban

---
+++ src/commands/shorts/cban.js
@@ -0,0 +1,58 @@
+const {
+    Command,
+    Args,
+    Events,
+    MessageCommandContext,
+} = require('@sapphire/framework');
+const { Message } = require('discord.js');
+
+class CBanCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'cban',
+            aliases: ['chillban'],
+            description: 'Calls the vcban command on the chill guest room.',
+            preconditions: ['VcActionPerms'],
+            usage: '<member> [reason]',
+        });
+    }
+
+    /**
+     * @param { Message } message
+     * @param { Args } args
+     * @param { MessageCommandContext } ctx
+     */
+    async messageRun(message, args, ctx) {
+        const rawMember = await args.pickResult('member');
+        const reason = (await args.restResult('string')).unwrapOr(
+            'No reason provided.'
+        );
+
+        if (rawMember.isErr()) {
+            return this.container.utility.errReply(
+                message,
+                'You must provide a valid user to ban from a vc.'
+            );
+        }
+        const member = rawMember.unwrap();
+
+        if (member) {
+            if (
+                message.member.roles.highest.position <=
+                member.roles.highest.position
+            ) {
+                return this.container.utility.errReply(
+                    message,
+                    'You may not vc ban members with equal or higher roles than you.'
+                );
+            }
+        }
+
+        message.content = `${ctx.prefix}vcban 1275177183064752128 ${member.id} ${reason}`;
+
+        this.container.client.emit(Events.PreMessageParsed, message);
+    }
+}
+
+module.exports = { CBanCommand };
