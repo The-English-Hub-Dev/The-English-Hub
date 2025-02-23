30eb0ceacb12259992635789939c2257dc132806
graviscool <88552913+graviscool@users.noreply.github.com>
2025-02-23 15:19:11 -0500

    feat(cmds): +fban

---
+++ src/commands/shorts/fban.js
@@ -0,0 +1,57 @@
+const {
+    Command,
+    Args,
+    Events,
+    MessageCommandContext,
+} = require('@sapphire/framework');
+const { Message } = require('discord.js');
+
+class FBanCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'fban',
+            description: 'Calls the vcban command on the feminism guest room.',
+            preconditions: ['NotOrigCmdChannel', 'VcActionPerms'],
+            usage: '<member> [reason]',
+        });
+    }
+
+    /**
+     * @param { Message } message
+     * @param { Args } args
+     * @param { MessageCommandContext } ctx
+     */
+    async messageRun(message, args, ctx) {
+        const rawMember = await args.pickResult('member');
+        const reason = (await args.restResult('string')).unwrapOr(
+            'No reason provided.'
+        );
+
+        if (rawMember.isErr()) {
+            return this.container.utility.errReply(
+                message,
+                'You must provide a valid user to ban from a vc.'
+            );
+        }
+        const member = rawMember.unwrap();
+
+        if (member) {
+            if (
+                message.member.roles.highest.position <=
+                member.roles.highest.position
+            ) {
+                return this.container.utility.errReply(
+                    message,
+                    'You may not vc ban members with equal or higher roles than you.'
+                );
+            }
+        }
+
+        message.content = `${ctx.prefix}vcban 1337490870567440435 ${member.id} ${reason}`;
+
+        this.container.client.emit(Events.PreMessageParsed, message);
+    }
+}
+
+module.exports = { FBanCommand };
