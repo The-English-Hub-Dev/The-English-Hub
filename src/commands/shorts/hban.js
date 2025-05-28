9c3ac0ccaaac1658489ac81cd90efd261614c77a
graviscool <88552913+graviscool@users.noreply.github.com>
2025-05-27 20:00:28 -0700

    feat(cmds): +hban, hunban

---
+++ src/commands/shorts/hban.js
@@ -0,0 +1,59 @@
+const {
+    Command,
+    Args,
+    Events,
+    MessageCommandContext,
+} = require('@sapphire/framework');
+const { Message } = require('discord.js');
+
+class HBanCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'hban',
+            aliases: ['healthban'],
+            description:
+                'Calls the vcban command on the mental health cafe guest room.',
+            preconditions: ['VcActionPerms'],
+            usage: '<member> [reason]',
+        });
+    }
+
+    /**
+     * @param { Message } message
+     * @param { Args } args
+     * @param { MessageCommandContext } ctx
+     */
+    async messageRun(message, args, ctx) {
+        const rawMember = await args.pickResult('member');
+        const reason = (await args.restResult('string')).unwrapOr(
+            'No reason provided.'
+        );
+
+        if (rawMember.isErr()) {
+            return this.container.utility.errReply(
+                message,
+                'You must provide a valid user to ban from a vc.'
+            );
+        }
+        const member = rawMember.unwrap();
+
+        if (member) {
+            if (
+                message.member.roles.highest.position <=
+                member.roles.highest.position
+            ) {
+                return this.container.utility.errReply(
+                    message,
+                    'You may not vc ban members with equal or higher roles than you.'
+                );
+            }
+        }
+
+        message.content = `${ctx.prefix}vcban 1375597364890308770 ${member.id} ${reason}`;
+
+        this.container.client.emit(Events.PreMessageParsed, message);
+    }
+}
+
+module.exports = { HBanCommand };
