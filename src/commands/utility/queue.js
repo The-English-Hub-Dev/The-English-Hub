d1e8ea056aa46b3bc3e53995d1bf1af264578fdc
graviscool <88552913+graviscool@users.noreply.github.com>
2025-12-14 22:36:00 -0500

    feat(queue): Update queue command to allow for restoring previous queue's with their id

--- src/commands/utility/queue.js
+++ src/commands/utility/queue.js
@@ -1,4 +1,4 @@
-const { Command } = require('@sapphire/framework');
+const { Command, Args } = require('@sapphire/framework');
 const {
     Message,
     EmbedBuilder,
@@ -13,7 +13,9 @@ class QueueCommand extends Command {
             ...options,
             name: 'queue',
             aliases: ['createqueue', 'create-queue'],
-            description: 'Creates a queue of users for use in vcs and events',
+            description:
+                'Creates or view a queue of users for use in vcs and events',
+            usage: ['<id type:DiscordSnowflake>', ''],
             preconditions: ['EventManager'],
         });
     }
@@ -21,10 +23,17 @@ class QueueCommand extends Command {
     /**
      *
      * @param { Message } message
+     * @param { Args } args
      * @returns
      */
-    async messageRun(message) {
-        const queueSnowflake = DiscordSnowflake.generate();
+    async messageRun(message, args) {
+        const queueID = await args.restResult('string');
+
+        let queueSnowflake = DiscordSnowflake.generate();
+
+        if (queueID.isOk()) {
+            queueSnowflake = queueID.unwrap();
+        }
 
         const queueEmbed = new EmbedBuilder()
             .setColor('Random')
@@ -47,12 +56,46 @@ class QueueCommand extends Command {
                 .setStyle(ButtonStyle.Secondary),
         ]);
 
-        await message.channel.send({
+        const m = await message.channel.send({
             embeds: [queueEmbed],
             components: [queueActionRow],
         });
 
-        message
+        if (queueID.isOk()) {
+            const oldEmbed = m.embeds[0];
+            const currentQueueUsersIDs = await this.container.redis.lrange(
+                `queue_${queueID.unwrap()}`,
+                0,
+                -1
+            );
+            const currentQueueUsers = currentQueueUsersIDs
+                .map(
+                    (id, index) =>
+                        `${currentQueueUsersIDs.length - index}: <@${id}>`
+                )
+                .reverse()
+                .join('\n');
+
+            const newEmbed = new EmbedBuilder()
+                .setColor('Random')
+                .setTitle(oldEmbed.title)
+                .setDescription(
+                    `Queue ID: ${queueSnowflake}\n\n**${currentQueueUsersIDs.length} User${
+                        currentQueueUsersIDs.length > 1 ? 's' : ''
+                    } in queue:**\n ${
+                        currentQueueUsers.length > 0
+                            ? currentQueueUsers
+                            : 'None'
+                    }`
+                )
+                .setFooter(oldEmbed.footer);
+            await m.edit({ embeds: [newEmbed] });
+            return message
+                .reply('Queue restored from ID!')
+                .then((m) => setTimeout(() => m.delete(), 5000));
+        }
+
+        await message
             .reply('Queue created!')
             .then((m) => setTimeout(() => m.delete(), 5000));
 
