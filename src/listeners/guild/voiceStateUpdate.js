b995f88504db5dae0aca0c0e98f19cdb7f010b25
graviscool <graviscool@users.noreply.github.com>
2026-02-04 20:26:00 +0000

    chore(formatting[auto]): run prettier on code

--- src/listeners/guild/voiceStateUpdate.js
+++ src/listeners/guild/voiceStateUpdate.js
@@ -120,11 +120,11 @@ class VoiceStateUpdateListener extends Listener {
         const userId = member.id;
         const inTarget = Boolean(
             newState.channelId &&
-                this.cameraOnChannelsSet.has(newState.channelId)
+            this.cameraOnChannelsSet.has(newState.channelId)
         );
         const wasInTarget = Boolean(
             oldState.channelId &&
-                this.cameraOnChannelsSet.has(oldState.channelId)
+            this.cameraOnChannelsSet.has(oldState.channelId)
         );
 
         // User joined a camera-required channel
